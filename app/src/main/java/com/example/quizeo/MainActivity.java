76ef3d4dcce9b749ceae6094492823f67226cf5f
Hakim-Agni <hakim.a@kpnmail.nl>
2021-03-11 15:26:21 +0100

    Made the home screen, buttons not functional

--- app/src/main/java/com/example/quizeo/MainActivity.java
+++ app/src/main/java/com/example/quizeo/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.quizeo;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
@@ -18,18 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.homescreen);
     }
 
     @Override
@@ -53,4 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void exitClick(View v) {
+        System.exit(0);
+    }
+
 }
