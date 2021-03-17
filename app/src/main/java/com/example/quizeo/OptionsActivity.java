49a3fbed0b187d6d33c26097d046c0b6df90bf73
Hakim-Agni <hakim.a@kpnmail.nl>
2021-03-17 17:05:22 +0100

    Added comments and removed redundant methods

--- app/src/main/java/com/example/quizeo/OptionsActivity.java
+++ app/src/main/java/com/example/quizeo/OptionsActivity.java
@@ -19,6 +19,7 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 public class OptionsActivity extends AppCompatActivity {
 
+    // default values for the options
     boolean sound;
     boolean verified;
     boolean darkmode;
@@ -30,8 +31,11 @@ public class OptionsActivity extends AppCompatActivity {
         setContentView(R.layout.options);
         Intent i = getIntent();
 
+        // get options from home activity
         getOptions(i);
+        // if darkmode has no changed
         if (!i.getBooleanExtra("darkmodeChange", false)) {
+            // set all options to the right state
             if (sound) {
                 unmute(findViewById(R.id.muteIcon));
             } else {
@@ -40,7 +44,8 @@ public class OptionsActivity extends AppCompatActivity {
             verifiedQuizzes(verified);
             friendRequests(request);
             darkMode(darkmode);
-        } else {
+        } else {    // if darkmode has changed
+            // get new options and set all options to the right state
             if (i.getBooleanExtra("soundNow", true)) {
                 unmute(findViewById(R.id.muteIcon));
             } else {
@@ -54,20 +59,20 @@ public class OptionsActivity extends AppCompatActivity {
     }
 
     @Override
-   public void onBackPressed() {
+    // Back button on device acts the same as cancel button
+    public void onBackPressed() {
         cancelOptions(findViewById(R.id.cancelButton));
     }
 
+    // Get the options set by the home activity
     public void getOptions(Intent i) {
         sound = i.getBooleanExtra("sound", true);
-
         verified = i.getBooleanExtra("verified", true);
-
         request = i.getBooleanExtra("request", false);
-
         darkmode = i.getBooleanExtra("darkmode", false);
     }
 
+    // When the user pressed the cancel button, changes to the options are not saved
     public void cancelOptions(View v) {
         // check if any options changed
         boolean soundchange = (findViewById(R.id.soundIcon).getVisibility() == View.VISIBLE) != sound;
@@ -83,6 +88,7 @@ public class OptionsActivity extends AppCompatActivity {
         }
     }
 
+    // Change all options back to the default state and return to the home activity
     public void changeOptionsExit() {
         // change options back
         if ((findViewById(R.id.soundIcon).getVisibility() == View.VISIBLE) != sound) {
@@ -97,9 +103,11 @@ public class OptionsActivity extends AppCompatActivity {
         if ((findViewById(R.id.darkModeCheck).getVisibility() == View.VISIBLE) != darkmode) {
             toggleDarkMode(findViewById(R.id.darkModeButton));
         }
+        // return to home activity
         openHome(sound, verified, darkmode, request);
     }
 
+    // Alerts user that the changes are not saved
     public void alertUser (View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
         builder.setMessage("Changes are not saved").setPositiveButton("Quit anyway", dialogClickListener2)
@@ -110,27 +118,29 @@ public class OptionsActivity extends AppCompatActivity {
         @Override
         public void onClick(DialogInterface dialog, int which) {
             switch (which){
-                case DialogInterface.BUTTON_POSITIVE:
-                    // Yes button clicked
+                case DialogInterface.BUTTON_POSITIVE:   // quit anyway button clicked
+                    // change options back and return to home activity
                     changeOptionsExit();
 
-                case DialogInterface.BUTTON_NEGATIVE:
-                    // No button clicked
+                case DialogInterface.BUTTON_NEGATIVE:   // return to option button clicked
+                    // return to options activity
                     break;
             }
         }
     };
 
+    // When the user pressed the save button, the changes are saved and the user returns to home
     public void saveOptions(View v) {
         // save current options
         boolean newSound = findViewById(R.id.soundIcon).getVisibility() == View.VISIBLE;
         boolean newVerified = findViewById(R.id.verifiedCheck).getVisibility() == View.VISIBLE;
         boolean newDarkmode = findViewById(R.id.darkModeCheck).getVisibility() == View.VISIBLE;
         boolean newRequest = findViewById(R.id.friendRequestCheck).getVisibility() == View.VISIBLE;
-        // return to home screen
+        // return to home activity
         openHome(newSound, newVerified, newDarkmode, newRequest);
     }
 
+    // Reads the input options and send them to the home activity
     public void openHome(boolean sound, boolean verified, boolean darkmode, boolean request) {
         Intent i = new Intent(this, MainActivity.class);
         i.putExtra("sound", sound);
@@ -140,17 +150,20 @@ public class OptionsActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    // Checks whether sound is on or off and mutes or unmutes accordingly
     public void alterSound(View v) {
         // get the current text of the button
         String s = ((Button)v).getText().toString();
-        // if the text says "Mute Sounds", mute, otherwise unmute
-        if (s.equals("Mute Sounds")) {
+        if (s.equals("Mute Sounds")) {           // if the text says "Mute Sounds"
+            // call mute method
             mute(this.findViewById(R.id.soundIcon));
-        } else if (s.equals("Play Sounds")) {
+        } else if (s.equals("Play Sounds")) {    // if the text says "Play Sounds"
+            // call unmute method
             unmute(this.findViewById(R.id.muteIcon));
         }
     }
 
+    // Mutes the application and shows the right text and image
     public void mute(View v) {
         // hide sound icon
         ImageView sound = (ImageView) v;
@@ -171,6 +184,7 @@ public class OptionsActivity extends AppCompatActivity {
         }*/
     }
 
+    // Unmutes the application and shows the right text and image
     public void unmute(View v) {
         // hide mute icon
         ImageView mute = (ImageView) v;
@@ -191,94 +205,100 @@ public class OptionsActivity extends AppCompatActivity {
         }*/
     }
 
+    // Toggles whether only verified quizzes is on or off
     public void toggleVerified(View v) {
         // find the corresponding check mark
         ImageView i = this.findViewById(R.id.verifiedCheck);
-        // call method that shows or hides the check mark
-        boolean enable = checkMark(i);
-        verifiedQuizzes(enable);
+        // call method that turns this options on or off based on the check mark
+        verifiedQuizzes(!(i.getVisibility() == View.VISIBLE));
     }
 
+    // Turns only verified quizzes on or off
     public void verifiedQuizzes(boolean enable) {
+        // find the right check mark
         ImageView i = this.findViewById(R.id.verifiedCheck);
-        if (enable) {
+        if (enable) {       // if the options should be enabled
+            // make the check mark visible
             i.setVisibility(View.VISIBLE);
             //add code to enable
 
-        } else {
+        } else {            // if the options should be disabled
+            // make the check mark invisible
             i.setVisibility(View.INVISIBLE);
             // add code to disable
 
         }
     }
 
+    // Toggles whether dark mode is on or off
     public void toggleDarkMode(View v) {
         // find the corresponding check mark
         ImageView i = this.findViewById(R.id.darkModeCheck);
-        // call method that shows or hides the check mark
-        boolean enable = checkMark(i);
-        darkMode(enable);
+        // call method that turns this options on or off based on the check mark
+        darkMode(!(i.getVisibility() == View.VISIBLE));
     }
 
+    // Turns dark mode on or off
     public void darkMode(boolean enable) {
+        // find the right check mark
         ImageView i = this.findViewById(R.id.darkModeCheck);
+        // get the intent for this activity
         Intent in = getIntent();
+        // add the current options to the intent
         in.putExtra("darkmodeChange", true);
         in.putExtra("soundNow", findViewById(R.id.soundIcon).getVisibility() == View.VISIBLE);
         in.putExtra("verifiedNow", findViewById(R.id.verifiedCheck).getVisibility() == View.VISIBLE);
         in.putExtra("requestNow", findViewById(R.id.friendRequestCheck).getVisibility() == View.VISIBLE);
-        if (enable) {
+        in.putExtra("darkmodeNow", enable);
+        if (enable) {       // if the options should be turned on
+            // make the check mark visible
             i.setVisibility(View.VISIBLE);
-            //add code to enable
-            in.putExtra("darkmodeNow", true);
+            // change the background globe
             findViewById(R.id.globeOptions).setVisibility(View.INVISIBLE);
             findViewById(R.id.globeOptionsDark).setVisibility(View.VISIBLE);
+            // if dark mode is not on, turn dark mode on
             if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
             }
-        } else {
+        } else {            // if the option should be turned off
+            // make the check mark invisible
             i.setVisibility(View.INVISIBLE);
-            // add code to disable
-            in.putExtra("darkmodeNow", false);
+            // change the background globe
             findViewById(R.id.globeOptionsDark).setVisibility(View.INVISIBLE);
             findViewById(R.id.globeOptions).setVisibility(View.VISIBLE);
+            // if dark mode is turned on, turn dark mode off
             if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
         }
     }
 
+    // Toggles whether friend requests are on or off
     public void toggleFriendRequests(View v) {
         // find the corresponding check mark
         ImageView i = this.findViewById(R.id.friendRequestCheck);
-        // call method that shows or hides the check mark
-        boolean enable = checkMark(i);
-        friendRequests(enable);
+        // call method that turns this options on or off based on the check mark
+        friendRequests(!(i.getVisibility() == View.VISIBLE));
     }
 
+    // Turns friend requests on or off
     public void friendRequests(boolean enable) {
+        // find the right check mark
         ImageView i = this.findViewById(R.id.friendRequestCheck);
-        if (enable) {
+        if (enable) {       // if the option should be turned on
+            // make the check mark visible
             i.setVisibility(View.VISIBLE);
             //add code to enable
 
-        } else {
+        } else {            // if the option should be turned off
+            // make the check mark invisible
             i.setVisibility(View.INVISIBLE);
             // add code to disable
 
         }
     }
 
-    public boolean checkMark(ImageView i) {
-        if (i.getVisibility() == View.VISIBLE) {
-            i.setVisibility(View.INVISIBLE);
-            return false;
-        } else {
-            i.setVisibility(View.VISIBLE);
-            return true;
-        }
-    }
-
+    // Asks the user if they want to quit the application
     public void exitClick(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
         builder.setMessage("Do you want to quit?").setPositiveButton("Quit", dialogClickListener)
@@ -289,13 +309,13 @@ public class OptionsActivity extends AppCompatActivity {
         @Override
         public void onClick(DialogInterface dialog, int which) {
             switch (which){
-                case DialogInterface.BUTTON_POSITIVE:
-                    // Yes button clicked
+                case DialogInterface.BUTTON_POSITIVE:       // Quit button clicked
+                    // close the application
                     finishAffinity();
                     break;
 
-                case DialogInterface.BUTTON_NEGATIVE:
-                    // No button clicked
+                case DialogInterface.BUTTON_NEGATIVE:       // Cancel button clicked
+                    // Return to options activity
                     break;
             }
         }
